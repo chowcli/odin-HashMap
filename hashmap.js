f58f58302f12eb6168468e6b767779268c53334c
chowcli <136085102+chowcli@users.noreply.github.com>
2024-03-03 10:48:18 +0700

    Finish hashmap class will not do the hashset because they are literally just the same Add support method for linkedList class

--- hashmap.js
+++ hashmap.js
@@ -10,6 +10,29 @@ class HashMap {
     this.#buckets = new Array(size).fill(null);
   }
 
+  #highLoadFactor() {
+    const storedKeys = this.keys().length;
+
+    return storedKeys / this.#buckets.length > this.#loadFactor;
+  }
+
+  #resize() {
+    const pairs = this.entries();
+
+    this.#buckets = new Array(this.#buckets.length * 2).fill(null);
+
+    pairs.forEach(([key, value]) => {
+      const hashCode = this.hash(key);
+      if (hashCode < 0 || hashCode >= this.#buckets.length) {
+        throw new Error("Trying to access index out of bound");
+      }
+
+      if (!this.#buckets[hashCode]) this.#buckets[hashCode] = new LinkedList();
+
+      this.#buckets[hashCode].append(key, value);
+    });
+  }
+
   hash(key) {
     let hashCode = 0;
     const bucketLength = this.#buckets.length;
@@ -25,6 +48,9 @@ class HashMap {
 
   set(key, value) {
     const hashCode = this.hash(key);
+    if (hashCode < 0 || hashCode >= this.#buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
 
     if (!this.#buckets[hashCode]) {
       this.#buckets[hashCode] = new LinkedList();
@@ -40,21 +66,98 @@ class HashMap {
 
       bucket.append(key, value);
     }
+
+    if (this.#highLoadFactor()) this.#resize();
   }
 
   get(key) {
     const bucket = this.#buckets[this.hash(key)];
+    const findNode = bucket.find(key);
 
-    if (bucket) return bucket.find(key);
-
-    return null;
+    return findNode ? findNode.value : null;
   }
 
   has(key) {
     const bucket = this.#buckets[this.hash(key)];
+    const findNode = bucket.find(key);
 
-    if (bucket) return Boolean(bucket.find(key));
+    return Boolean(findNode);
+  }
+
+  remove(key) {
+    const bucket = this.#buckets[this.hash(key)];
+
+    if (bucket && bucket.find(key)) {
+      bucket.remove(key);
+      return true;
+    }
 
     return false;
+  }
+
+  length() {
+    let storedKeys = 0;
+
+    this.#buckets.forEach(bucket => {
+      if (bucket) storedKeys += bucket.size;
+    });
+
+    return storedKeys;
+  }
+
+  clear() {
+    const size = 16;
+    this.#buckets = new Array(size).fill(null);
+  }
+
+  keys() {
+    const keysArr = [];
+
+    this.#buckets.forEach(bucket => {
+      if (bucket) {
+        let temp = bucket.head;
+
+        while (temp) {
+          keysArr.push(temp.key);
+          temp = temp.next;
+        }
+      }
+    });
+
+    return keysArr;
+  }
+
+  values() {
+    const values = [];
+
+    this.#buckets.forEach(bucket => {
+      if (bucket) {
+        let temp = bucket.head;
+
+        while (temp) {
+          values.push(temp.value);
+          temp = temp.next;
+        }
+      }
+    });
+
+    return values;
+  }
+
+  entries() {
+    const pairs = [];
+
+    this.#buckets.forEach(bucket => {
+      if (bucket) {
+        let temp = bucket.head;
+
+        while (temp) {
+          pairs.push([temp.key, temp.value]);
+          temp = temp.next;
+        }
+      }
+    });
+
+    return pairs;
   }
 }
--- linkedList.js
+++ linkedList.js
@@ -4,13 +4,16 @@ class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.size = 0;
   }
 
   append(key, value) {
     if (this.head) {
       this.tail = this.tail.next = new Node(key, value);
+      this.size++;
     } else {
       this.head = this.tail = new Node(key, value);
+      this.size++;
     }
   }
 
@@ -18,13 +21,33 @@ class LinkedList {
   find(key) {
     let temp = this.head;
 
-    while (temp.next) {
+    while (temp) {
       if (temp.key === key) return temp;
 
       temp = temp.next;
     }
 
     return null;
+  }
+
+  // Remove node with correct key
+  remove(key) {
+    const removedNode = this.find(key);
+
+    if (removedNode === this.head) {
+      this.head = this.head.next;
+      if (!this.head) this.tail = null; // if the list after update is empty
+      this.size--;
+    }
+
+    let temp = this.head;
+    while (temp.next !== removedNode) {
+      temp = temp.next;
+    }
+    temp.next = removedNode.next;
+
+    if (!removedNode.next) this.tail = temp; // if removedNode is also the tail
+    this.size--;
   }
 }
 
